7d462c7e8ffba4a44d0f6aa9dfc961afb1473c7c
pramodginuga <pramod.ginuga@gmail.com>
2022-05-29 16:35:36 +0530

    Initial Checkin

---
+++ DockerCompose/src/main/java/Selenium/DockerCompose/App.java
@@ -0,0 +1,8 @@
+package Selenium.DockerCompose;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ DockerCompose/src/test/java/Selenium/DockerCompose/AppTest.java
@@ -0,0 +1,12 @@
+package Selenium.DockerCompose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+    @Test
+    void assertjShouldWork() {
+        assertThat(true).isTrue();
+    }
+}
